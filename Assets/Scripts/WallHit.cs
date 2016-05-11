c5388043662eef30fcd96a715e9c64e0494cb061
Aram Joseph <ajoseph@utc4me.org>
2016-05-10 21:24:40 -0400

    wut eva

--- Assets/Scripts/WallHit.cs
+++ Assets/Scripts/WallHit.cs
@@ -3,11 +3,18 @@ using System.Collections;
 
 public class WallHit : MonoBehaviour
 {
+	[SerializeField]
+	Vector2 knockbackForce;
+
 
 	void OnCollisionEnter2D(Collision2D coll)
     {
         if (coll.gameObject.tag == "Player")
         {
+			coll.gameObject.GetComponentInParent<LevelScroll> ().enabled = false;
+			coll.gameObject.GetComponent<Bounce> ().enabled = false;
+			Time.timeScale = 0.01f;
+			coll.gameObject.GetComponent<Rigidbody2D> ().AddForce (knockbackForce);
             Debug.Log("Hit");
         }
     }
